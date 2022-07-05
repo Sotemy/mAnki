4c3dd0b5a5fe50a39aa4ed7b0a442b8d8762c8cc
moninger <moninger76131@gmail.com>
2022-07-05 23:08:20 +0200

    saving in localStorage

--- web-app/src/pages/Login.jsx
+++ web-app/src/pages/Login.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { Button, Container, Form, FormControl, Spinner } from 'react-bootstrap';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login, reset } from '../features/auth/authSlice';
 
 function Login() {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const user = JSON.parse(localStorage.getItem('user'))
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { isLoaded, isSuccess, isError, message} = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if(isError){
+        alert(message)
+    }
+
+    if(isSuccess || user){
+        let com = (isSuccess || user === true)
+        console.log(com)
+        navigate('/')
+    }
+
+    dispatch(reset())
+
+}, [user, isError, isSuccess, message, navigate, dispatch])
+  
+
+  const onChange = (e) => {
+    setFormData((prevState)=>({
+      ...prevState,
+        [e.target.id]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    dispatch(login(formData))
+}
+
+if(isLoaded){
   return (
-    <div>Login</div>
+      <div id="spinner-container">
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+      )
+}
+
+  return (
+    <Container>
+      <Form onSubmit={(e)=>onSubmit(e)}>
+        <FormControl placeholder='E-Mail' id="email" name="email" type="email" value={formData.email} onChange={(e)=>onChange(e)} />
+        <FormControl placeholder='Password' id="password" name="password" type="password" value={formData.password} onChange={(e)=>onChange(e)} />
+        <Button type="submit">Login</Button>
+      </Form>
+    </Container>
   )
 }
 
--- web-app/src/pages/Register.jsx
+++ web-app/src/pages/Register.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { useState } from 'react'
+import { Button, Container, Form, FormControl } from 'react-bootstrap'
 
 function Register() {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const onChange = (e) => {
+    setFormData((prevState)=>({
+      ...prevState,
+        [e.target.id]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(formData)
+}
+
   return (
-    <div>Register</div>
+    <Container>
+      <Form onSubmit={(e)=>onSubmit(e)}>
+        <FormControl placeholder='E-Mail' id="email" name="email" type="email" value={formData.email} onChange={(e)=>onChange(e)} />
+        <FormControl placeholder='Password' id="password" name="password" type="password" value={formData.password} onChange={(e)=>onChange(e)} />
+        <FormControl placeholder='Password again' id="password2" name="password2" type="password" value={formData.password2} onChange={(e)=>onChange(e)} />
+        <Button type="submit">Login</Button>
+      </Form>
+    </Container>
   )
 }
 
