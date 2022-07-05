c76ce96e37720f668a7816890d22eb7ffbcad736
moninger <moninger76131@gmail.com>
2022-07-05 23:07:54 +0200

    a little bit design

--- web-app/src/components/Navigation.jsx
+++ web-app/src/components/Navigation.jsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import { Navbar, Nav, Container } from "react-bootstrap"
-
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Navigation() {
+
+  const user=true
+  
   return (
     <Navbar bg="dark" variant="dark">
-        <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        </Container>
-    </Navbar>
+    <Container>
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="me-auto">
+
+        <LinkContainer to="/">
+          <Nav.Link>Home</Nav.Link>
+        </LinkContainer>
+
+      </Nav>
+      <Nav className="mr-auto">
+      { user === true ? (
+      
+      <LinkContainer to="#profile">
+        <Nav.Link>profile</Nav.Link>
+      </LinkContainer>
+      ) : (
+          <>
+          <LinkContainer to="/login">
+            <Nav.Link>Login</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/register">
+            <Nav.Link>Register</Nav.Link>
+          </LinkContainer>
+          </>
+      ) 
+      }
+        
+      </Nav>
+    </Container>
+  </Navbar>
   )
 }
 
--- web-app/src/pages/Home.jsx
+++ web-app/src/pages/Home.jsx
@@ -1,8 +1,134 @@
 import React from 'react'
+import { Container, ListGroup, Row, Col } from 'react-bootstrap'
 
 function Home() {
+
+
+
   return (
-    <div>Home</div>
+    <div className="home">
+      <Container>
+        <ListGroup variant="flush">
+
+          <ListGroup.Item>
+            <Row>
+              <Col>
+
+                Cras justo odio
+
+              </Col>
+              <Col>
+                <div className="badge_cards_container">
+                  <Row>
+
+                    <Col>
+                      <p className='new_badge'>20</p>
+                    </Col>
+
+                    <Col>
+                      <p className='bad_badge'>20</p>
+                    </Col>
+
+                    <Col>
+                      <p className='mature_badge'>20</p>
+                    </Col>
+
+                  </Row>
+                </div>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+
+          <ListGroup.Item>
+            <Row>
+              <Col>
+
+                Cras justo odio
+
+              </Col>
+              <Col>
+                <div className="badge_cards_container">
+                  <Row>
+
+                    <Col>
+                      <p className='new_badge'>20</p>
+                    </Col>
+
+                    <Col>
+                      <p className='bad_badge'>20</p>
+                    </Col>
+
+                    <Col>
+                      <p className='mature_badge'>20</p>
+                    </Col>
+
+                  </Row>
+                </div>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+
+          <ListGroup.Item>
+            <Row>
+              <Col>
+
+                Cras justo odio
+
+              </Col>
+              <Col>
+                <div className="badge_cards_container">
+                  <Row>
+
+                    <Col>
+                      <p className='new_badge'>20</p>
+                    </Col>
+
+                    <Col>
+                      <p className='bad_badge'>20</p>
+                    </Col>
+
+                    <Col>
+                      <p className='mature_badge'>20</p>
+                    </Col>
+
+                  </Row>
+                </div>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+
+          <ListGroup.Item>
+            <Row>
+              <Col>
+
+                Cras justo odio
+
+              </Col>
+              <Col>
+                <div className="badge_cards_container">
+                  <Row>
+
+                    <Col>
+                      <p className='new_badge'>20</p>
+                    </Col>
+
+                    <Col>
+                      <p className='bad_badge'>20</p>
+                    </Col>
+
+                    <Col>
+                      <p className='mature_badge'>20</p>
+                    </Col>
+
+                  </Row>
+                </div>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+
+        </ListGroup>
+      </Container>
+    </div>
   )
 }
 
